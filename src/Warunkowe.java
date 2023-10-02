967a43e0e262b306486beaf8c5533604a008e02a
bartek-kaczmarek <bartek.kaczmarek@gmail.com>
2023-10-02 12:39:00 +0200

    zadanie 13

---
+++ src/Warunkowe.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Warunkowe {
+    public static void main(String[] args) {
+        System.out.println("Podaj liczbę");
+        Scanner scanner = new Scanner(System.in);
+        int liczba = scanner.nextInt();
+
+        if (liczba>0){
+            System.out.println("Dodatnia");
+        }
+        if (liczba==0){
+            System.out.println("zero");
+        }
+        if (liczba<0){
+            System.out.println("ujemna");
+        }
+    }
+}
