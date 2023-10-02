af13c7dc2ab5fb749c3721d8f73260e992372a07
bartek-kaczmarek <bartek.kaczmarek@gmail.com>
2023-10-02 15:10:51 +0200

    zadanie 13

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int liczba = scanner.nextInt();
+        int playerChoice = 1;
+        int points = 0;
+        int wynik = 0;
+        do {
+            System.out.println("Chcesz rzucić kostką czy spasować?");
+            Random random = new Random();
+            wynik = random.nextInt(1, 7);
+            System.out.println("Twój wynik to: " + wynik);
 
-        System.out.println("Chcesz rzucić kostką czy spasować?");
 
+            System.out.println("1. rzucam");
+            System.out.println("2. pasuję");
+            playerChoice = scanner.nextInt();
+            if (wynik == 1) {
+                System.out.println("Przegrałeś!");
+            } else {
+
+                points = points + wynik;
+            }
+        } while ((points < 20));
+        System.out.println("Koniec gry - wygrałeś!");
     }
 }
--- src/Rzutkostka.java
+++ src/Rzutkostka.java
@@ -1,5 +1,20 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Rzutkostka {
     public static void main(String[] args) {
+        int playerChoice = 1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Chcesz rzucić kostką czy spasować?");
+        Random random = new Random();
+        int wynik = random.nextInt(1, 7);
+        System.out.println("");
+        System.out.println("Twój wynik to: " + wynik);
+
+        System.out.println("");
+        System.out.println("1. rzucam");
+        System.out.println("2. pasuję");
+        playerChoice = scanner.nextInt();
 
     }
 }
