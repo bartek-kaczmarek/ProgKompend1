5d0539ece24af4d59e0db28ce334e2358b8f7289
bartek-kaczmarek <bartek.kaczmarek@gmail.com>
2023-10-03 12:56:16 +0200

    zadanie 13

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,93 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        String[] pyt1 = {"1", "Jaka jest stolica Hiszpanii?", "Madryt"};
-        String[] pyt2 = {"2", "Jaki jest największy ocean?", "Pacyfik"};
 
-        String[][] pytania = {pyt1, pyt2};
+    static Scanner scanner = new Scanner(System.in);
+    static int pkt = 0;
 
-        Random random = new Random();
+    static boolean odpowiedzNaPytanie(String[] pytanie) {
 
-        int nrpyt = random.nextInt(pytania.length);
-        String[] aktpyt = pytania[nrpyt];
-        String odp;
-        int pkt=0;
-        Scanner scanner = new Scanner(System.in);
+        System.out.println(pytanie[0]);
 
+        String odp = scanner.next().toUpperCase();
+        String prawodp;
 
-        System.out.println("Pytanie nr " + aktpyt[0]);
-        System.out.println(aktpyt[1]);
-        odp = scanner.next();
-        if (odp.contains(aktpyt[2])) {
-            System.out.println("Odpowiedź prawidłowa - " + odp);
+        prawodp = pytanie[1];
+        prawodp = prawodp.toUpperCase();
+
+        if (odp.contains(prawodp)) {
+            System.out.println("Odpowiedź prawidłowa - " + pytanie[1]);
             pkt = pkt + 1;
 
         } else {
             System.out.println("WRONG");
+            return false;
         }
+        return true;
+    }
+
+    static String[][] uluzPytania() {
+
+        //  String[] pyt1 = {"Jaka jest stolica Hiszpanii?", "Madryt"};
+        //  String[] pyt2 = {"Jaki jest największy ocean?", "Pacyfik"};
+        // String[] pyt3 = {"Jaki kolor ma żaba?", "Zielony"};
+        // String[] pyt4 = {"Kto rządzi Wlk Brytanią?", "Król"};
+        // String[] pyt5 = {"Największa rzeka", "Amazonka"};
+        //  String[] pyt6 = {"Satelita Ziemi", "Księżyc"};
+        //  String[] pyt7 = {"Gdzie piramida", "Egipt"};
+        //  String[] pyt8 = {"Kto rządzi Wlk Brytanią?", "Król"};
+
+
+        //String[][] pytania = {pyt1, pyt2, pyt3, pyt4,pyt5, pyt6, pyt7, pyt8};
+
+        String[][] pytania = {
+                {"Jaka jest stolica Hiszpanii?", "Madryt"},
+                {"Jaki jest największy ocean?", "Pacyfik"},
+                {"Kto rządzi Wlk Brytanią?", "Król"},
+                {"Jaki kolor ma żaba?", "Zielony"},
+                {"Największa rzeka", "Amazonka"},
+                {"Satelita Ziemi", "Księżyc"},
+                {"Gdzie piramida", "Egipt"},
+                {"Kto rządzi Wlk Brytanią?", "Król"}
+        };
+
+        return pytania;
+    }
+
+    ;
+
+    public static void main(String[] args) {
+
+        String[][] pytania = uluzPytania();
+
+        int wybor;
+        boolean czyGra = true;
+        Random random = new Random();
+
+
+        do {
+
+            System.out.println("Do you want to play a game?");
+            System.out.println("1. TAK");
+            System.out.println("2. NIE");
+            wybor = scanner.nextInt();
+            if (wybor == 2) {
+                czyGra = false;
+                break;
+            }
+
+            int nrpyt = random.nextInt(pytania.length);
+            String[] aktpyt = pytania[nrpyt];
+
+            System.out.println("***********");
+            System.out.println("Pytanie nr " + (nrpyt + 1));
+
+
+            czyGra = odpowiedzNaPytanie(aktpyt);
+
+
+        } while (czyGra);
+
         System.out.println("Twój wynik to " + pkt);
 
     }
